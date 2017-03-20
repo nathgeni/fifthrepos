f75064ad11cfd7ed752dcb0066af06b9f8fef0de
Nathan <nath@email.com>
2017-03-20 14:28:43 +1030

    Changed cheque method value

--- feat_cheque1/BankingApp/BankingApp/Banking.xaml.cs
+++ feat_cheque1/BankingApp/BankingApp/Banking.xaml.cs
@@ -133,7 +133,7 @@ namespace BankingApp
                             break;
 
                         case "ChequeRadioButtonOn&SavingsRadioButtonOn":  // from Cheque to Savings
-                            fromBalance = fromBalance - amountTransfer;
+                            fromBalance = fromBalance - amountTransfer -2.0m;
                             cChequeBalance = fromBalance;
                             fromBalanceLabel.Content = cChequeBalance.ToString("C");
 
@@ -143,7 +143,7 @@ namespace BankingApp
                             break;
 
                         case "ChequeRadioButtonOn&InvestmentRadioButtonOn":   // from Cheque to Investment
-                            fromBalance = fromBalance - amountTransfer;
+                            fromBalance = fromBalance - amountTransfer -2.0m;
                             cChequeBalance = fromBalance;
                             fromBalanceLabel.Content = cChequeBalance.ToString("C");
 
